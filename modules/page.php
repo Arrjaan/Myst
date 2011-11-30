7cd256e5d92fd77370679d40e0c694b69efe5b24
Peter Postema <petermail_10@hotmail.com>
2011-11-30 22:49:14 +0100

    phpmyadmin link toegevoegd

--- modules/page.php
+++ modules/page.php
@@ -7,6 +7,8 @@ class Page {
 	function __construct($page = 'home') {
 		$db = new Mysqli("adfari.com","myserver_myst","eq,yQ_cL6-T}","myserver_myst");
 		echo $db->stat();
+		echo "<br /><br />";
+		echo 'Zie <a target="_blank" href="http://adfari.com/phpmyadmin">http://adfari.com/phpmyadmin</a> voor PhpMyAdmin.<br />';
 	}
 	function layout($l = 'default') {
 		$l = new Layout($l);
@@ -17,5 +19,7 @@ class Page {
 		eval("?>".$html);
 	}
 }
+
+new page;
 
 ?>
