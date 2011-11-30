e61fce6e01a450390e25ee23e0a4f9b67e7b369e
Arjan <arjanspoelstra@live.nl>
2011-11-30 22:44:50 +0100

    page.php verder uitgebreid.

--- modules/page.php
+++ modules/page.php
@@ -1,11 +1,22 @@
 <?php
 
 class Page {
+	var $html;
+	var $content;
+	
 	function __construct($page = 'home') {
 		$db = new Mysqli("adfari.com","myserver_myst","eq,yQ_cL6-T}","myserver_myst");
 		echo $db->stat();
 		echo "<br /><br />";
 		echo 'Zie <a target="_blank" href="http://adfari.com/phpmyadmin">http://adfari.com/phpmyadmin</a> voor PhpMyAdmin.<br />';
+	}
+	function layout($l = 'default') {
+		$l = new Layout($l);
+		$html = $l->getHTML(); // Zorg voor een class Layout die bij de functie getHTML alle HTML voor de pagina laat zien.
+	}
+	function show() {
+		extract($content);
+		eval("?>".$html);
 	}
 }
 
