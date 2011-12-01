269b20964e723fbe9efc97fd8703c968051fcf73
Peter Postema <petermail_10@hotmail.com>
2011-12-01 22:36:41 +0100

    gastenboek 0.0.2:versie en titel werkend. modulefuncies 0.0.2:head en bottom werkend met php
    q
    end

--- modules/gastenboek.php
+++ modules/gastenboek.php
@@ -1,12 +1,13 @@
 <?php
 // module gastenboek
-class guestbook {
-	public $content = array("titel"=>"gastenboek","content"=>"hoi");
-}
+	$content = array("title"=>"gastenboek","version"=>"0.0.2");
 
 include("module-functies.php");
 
 $d = new head;
-$d->show();
+$d->show($content['title']);
 
+
+$e = new bottom;
+$e->show($content['version']);
 ?>
--- modules/module-functies.php
+++ modules/module-functies.php
@@ -1,24 +1,53 @@
 <?php
 // module-functies \\
+	$content = array("title"=>"module-functies","version"=>"0.0.2");
 
 class head {
 	public $start = '
 		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 		<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 			<head>
-				<title>help</title>
+				<title><?php echo $title; ?></title>
+				<style type="text/css">
+				    * {
+						margin: 0;
+					}
+					html, body {
+						height: 100%;
+					}
+					.wrapper {
+						min-height: 100%;
+						height: auto !important;
+						height: 100%;
+						margin: 0 auto -2em;
+					}
+					.footer, .push {
+						margin-left: 1em;
+						height: 2em;
+					}
+				</style>
 			</head>
 
-		<body>';
+		<body>
+			<div class="wrapper">';
 		
-	function show() {
-		echo $start;
+	function show($title) {
+		eval ("?>".$this->start);
 	}
 }
 
 class bottom {
 	public $end = '
+				<div class="push"></div>
+			</div>
+		<div class="footer">
+			Versie: <?php echo $version; ?>
+		</div>
 		</body>
 		</html>';
+
+	function show($version) {
+		eval ("?>".$this->end);
+	}		
 }
 ?>
