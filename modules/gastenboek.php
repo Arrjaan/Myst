3b03ae6a0eb2b1d887ec904df827367cff5664c3
Peter Postema <petermail_10@hotmail.com>
2011-12-01 21:18:36 +0100

    uitprobeersel

--- modules/gastenboek.php
+++ modules/gastenboek.php
@@ -1,9 +1,12 @@
 <?php
 // module gastenboek
 class guestbook {
-
+	public $content = array("titel"=>"gastenboek","content"=>"hoi");
 }
 
+include("module-functies.php");
 
+$d = new head;
+$d->show();
 
 ?>
