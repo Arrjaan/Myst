c448d8c8bdc7301b6ed4b1d6896efbf4eeeb3ba3
Peter Postema <petermail_10@hotmail.com>
2012-04-09 17:38:46 +0200

    kleine aanpassing in db gegevens (local>unlocal)

--- config.inc.php
+++ config.inc.php
@@ -5,12 +5,9 @@ $config['home'] = 'index';
 $config['rootdir'] = '/';
 
 // database gegevens \\
-$db['server'] = '127.0.0.1';
-//$db['server'] = 'sql09.freemysql.net';
-$db['user'] = 'root';
-//$db['user'] = 'projectmyst';
-$db['passw'] = 'd156441';
-//$db['passw'] = 'GitMyst';
+$db['server'] = 'sql09.freemysql.net';
+$db['user'] = 'projectmyst';
+$db['passw'] = 'GitMyst';
 $db['db'] = 'projectmyst';
 $db['port'] = 3306;
 
